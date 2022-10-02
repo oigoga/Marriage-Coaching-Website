fbeae7c9512a4dd7d76822e3b389e55effc49baf
oigoga <gogaelisabeth21@gmail.com>
2022-10-02 14:35:44 +0100

    Adding all the files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+ 
+    content: ["./src/index.html"],
+    theme: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 640px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
+  
